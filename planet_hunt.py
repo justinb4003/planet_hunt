7472a79588c9dde3b583353cf52c3113c4e827cd
Justin Buist <justinb@countrysidegreenhouse.com>
2019-11-03 15:51:02 -0500

    Refactor and it compares to accepted data

--- planet_hunt.py
+++ planet_hunt.py
@@ -2,10 +2,20 @@
 
 import sys
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from math import ceil
 # from scipy.signal import argrelextrema
+
+
+def get_answer_filename():
+    return 'kepler_accepted_results.csv'
+
+
+def get_datafile_filename(kepid):
+    kepstr = '{:09d}'.format(kepid)
+    return 'data/KIC{}.tbl'.format(kepstr)
 
 
 # PROBLEM:  Having to futz with this percentile between runs
@@ -36,11 +46,11 @@ def strip_outliers(lst, m=2):
 def find_transit_period(vx, vy):
     # This gives us the time diff between each peak, some of which will be over
     # gaps in time where there is no data
-    diffs = np.diff(valley_x)
+    diffs = np.diff(vx)
     # We start knowing that at least every valley is when a transition occured
-    valley_count = len(valley_x)
-    last_trans = valley_x[-1]
-    first_trans = valley_x[0]
+    valley_count = len(vx)
+    last_trans = vx[-1]
+    first_trans = vx[0]
     # Take a rough guess at the period.  This won't work if the period where
     # there are gaps are longer than the periods of obvservation.
     # TODO: Fix that.  That's gotta get fixed.
@@ -73,7 +83,7 @@ def find_transit_period(vx, vy):
 # Shift every X back in time until it's relative position to transition always
 # puts peak transition at time = 0.000
 def phase_shift(first_transit_time, origx, origy, P):
-    origx -= valley_x[0]
+    origx -= first_transit_time
     newx = []
     for i in origx:
         # Guarantee I'm doing something stupid here, not seeing it because I'm
@@ -92,7 +102,6 @@ def phase_shift(first_transit_time, origx, origy, P):
 # find_transit_valley(...) that works.
 # TODO: Figure that out automatically
 """
-_, allx, ally = np.loadtxt('data/KIC002571238.tbl', unpack=True, skiprows=3)  # 0.5
 """
 _, allx, ally = np.loadtxt('data/KIC007950644.tbl', unpack=True, skiprows=3)  # 0.5
 _, allx, ally = np.loadtxt('data/KIC009631995.tbl', unpack=True, skiprows=3)  # 0.5
@@ -109,30 +118,56 @@ _, allx, ally = np.loadtxt('data/KIC010418224.tbl', unpack=True, skiprows=3)
 _, allx, ally = np.loadtxt('data/KIC011853905.tbl', unpack=True, skiprows=3)
 """
 
-valley_x, valley_y = find_transit_valley(allx, ally, 0.5)  # <-- magic % value
-show_valley_graph = False
-if show_valley_graph:  # A debbuging graph showing where we think transits are
-    plt.scatter(allx, ally, s=1)
-    plt.scatter(valley_x, valley_y, s=3)
-    plt.xlabel("Julian Days")
-    plt.ylabel("Lc Intensity")
-    plt.show()
-    sys.exit(0)
 
-P = find_transit_period(valley_x, valley_y)
-allx, ally = phase_shift(valley_x, allx, ally, P)
+def calculate_result(kepid, percentile, show_valley=False, show_result=False):
+    """
+    kepid: Kepler Object ID (star) that we're looking at.
+    percentile: Kind of a magic number at this point, generally 0.5-3%
+                is where we make the threshold for a valley being legit
+    TODO: This only looks for a single object, the largest one.
+    """
+    _, allx, ally = np.loadtxt(get_datafile_filename(kepid),
+                               unpack=True,
+                               skiprows=3)
+    valley_x, valley_y = find_transit_valley(allx, ally, percentile)
 
-# Take a running mean of N size for each list
-N = 551  # <-- magic number found via fiddling
-meanx = np.convolve(allx, np.ones((N,))/N, mode='valid')[(N-1):]
-meany = np.convolve(ally, np.ones((N,))/N, mode='valid')[(N-1):]
+    if show_valley:  # A debbuging graph showing where we think transits are
+        plt.scatter(allx, ally, s=1)
+        plt.scatter(valley_x, valley_y, s=3)
+        plt.xlabel("Julian Days")
+        plt.ylabel("Lc Intensity")
+        plt.show()
+
+    P = find_transit_period(valley_x, valley_y)
+    allx, ally = phase_shift(valley_x[0], allx, ally, P)
+
+    # Take a running mean of N size for each list
+    N = 551  # <-- magic number found via fiddling
+    meanx = np.convolve(allx, np.ones((N,))/N, mode='valid')[(N-1):]
+    meany = np.convolve(ally, np.ones((N,))/N, mode='valid')[(N-1):]
+
+    # If the graph is a scatter showing a nice U with a magenta line cutting
+    # right through it you've got a planet.
+    if show_result:
+        plt.scatter(allx, ally, s=1)
+        plt.plot(meanx, meany, "m")
+        # plt.scatter(valley_x, valley_y, marker='o', color='green', label='Valleys')
+        plt.xlabel("Julian Days")
+        plt.ylabel("Lc Intensity")
+        plt.show()
+    # Eventually the function will return more but for now just the period
+    return P
 
 
-# If the graph is a scatter showing a nice U with a magenta line cutting
-# right through it showing the pattern of transit covering you've got a planet.
-plt.scatter(allx, ally, s=1)
-plt.plot(meanx, meany, "m")
-# plt.scatter(valley_x, valley_y, marker='o', color='green', label='Valleys')
-plt.xlabel("Julian Days")
-plt.ylabel("Lc Intensity")
-plt.show()
+def get_accepted_result(kepid):
+    df = pd.read_csv(get_answer_filename(), skiprows=31)
+    df = df.set_index('kepid')
+    ap = df.loc[kepid]['koi_period']
+    print("ap: {}".format(ap))
+    return ap
+
+
+p1 = calculate_result(2571238, 0.5, show_valley=False, show_result=False)
+real_p1 = get_accepted_result(2571238)
+diff = abs(real_p1 - p1)
+print("Error in period: {}".format(diff))
