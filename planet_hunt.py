c2a3c7c8e55be8876f1cfe6bbee7df929cf39c2b
Justin Buist <justinb@countrysidegreenhouse.com>
2019-11-03 09:01:56 -0500

    Change a few things as I clean up

--- planet_hunt.py
+++ planet_hunt.py
@@ -8,9 +8,12 @@ from math import ceil
 # from scipy.signal import argrelextrema
 
 
-def find_transit_valley(x, y):
+# PROBLEM:  Having to futz with this percentile between runs
+# .... obviously there's a better stats method to go about narrowing
+# this down.
+def find_transit_valley(x, y, percentile):
     valleys = find_peaks(y*-1, distance=10)[0]
-    thresh = np.percentile(y, 3)
+    thresh = np.percentile(y, percentile)
     print("Threshold: {}".format(thresh))
     # There's a way to do this with list expressions; speedier that way
     good_valleys = []
@@ -20,8 +23,14 @@ def find_transit_valley(x, y):
             good_valleys.append(idx)
     valley_x = x[good_valleys]
     valley_y = y[good_valleys]
-    # The darkest 3% will be considered candidates for transition
     return valley_x, valley_y
+
+
+def strip_outliers(lst, m=2):
+    d = np.abs(lst - np.median(lst))
+    mdev = np.median(d)
+    s = d / (mdev if mdev else 1.0)
+    return lst[s < m]
 
 
 def find_transit_period(vx, vy):
@@ -36,6 +45,11 @@ def find_transit_period(vx, vy):
     # there are gaps are longer than the periods of obvservation.
     # TODO: Fix that.  That's gotta get fixed.
     P = (last_trans - first_trans) / (valley_count - 1)
+    print("First stab at P: {}".format(P))
+    diffs = strip_outliers(diffs)
+    P = np.mean(diffs)
+    print("Second stab at P: {}".format(P))
+
     for d in diffs:
         if d > P:
             # If the time diff between valleys here is more than the average
@@ -53,6 +67,9 @@ def find_transit_period(vx, vy):
     print("Period: {}".format(P))
     return P
 
+
+# Shift every X back in time until it's relative position to transition always
+# puts peak transition at time = 0.000
 def phase_shift(first_transit_time, origx, origy, P):
     origx -= valley_x[0]
     newx = []
@@ -66,30 +83,51 @@ def phase_shift(first_transit_time, origx, origy, P):
     newx, newy = zip(*sorted(zip(newx, origy)))
     return newx, newy
 
+
 # Solid cases
-_, allx, ally = np.loadtxt('data/KIC006922244.tbl', unpack=True, skiprows=3)
 """
-_, allx, ally = np.loadtxt('data/KIC008359498.tbl', unpack=True, skiprows=3)
+# comment at end of line indicates values of 'percentile' to pass to
+# find_transit_valley(...) that works.
+# TODO: Figure that out automatically
+_, allx, ally = np.loadtxt('data/KIC006922244.tbl', unpack=True, skiprows=3) # 3
+_, allx, ally = np.loadtxt('data/KIC008359498.tbl', unpack=True, skiprows=3) # 3
+_, allx, ally = np.loadtxt('data/KIC002571238.tbl', unpack=True, skiprows=3) # 0.5
+_, allx, ally = np.loadtxt('data/KIC007950644.tbl', unpack=True, skiprows=3) # 0.5
+_, allx, ally = np.loadtxt('data/KIC009631995.tbl', unpack=True, skiprows=3) # 0.5
 """
 
 # Not working yet.
 """
-_, allx, ally = np.loadtxt('data/KIC002571238.tbl', unpack=True, skiprows=3)
+# I think this one is a triple system or binary in resonance
+.... or I'm a doof that doesn't understand the data.  All possible.
 _, allx, ally = np.loadtxt('data/KIC005881688.tbl', unpack=True, skiprows=3)
-_, allx, ally = np.loadtxt('data/KIC007950644.tbl', unpack=True, skiprows=3)
-_, allx, ally = np.loadtxt('data/KIC009631995.tbl', unpack=True, skiprows=3)
+"""
 _, allx, ally = np.loadtxt('data/KIC010418224.tbl', unpack=True, skiprows=3)
+"""
 _, allx, ally = np.loadtxt('data/KIC011853905.tbl', unpack=True, skiprows=3)
 """
 
-valley_x, valley_y = find_transit_valley(allx, ally)
+valley_x, valley_y = find_transit_valley(allx, ally, 2.5)
+show_valley_graph = False
+if show_valley_graph:
+    plt.scatter(allx, ally, s=1)
+    plt.scatter(valley_x, valley_y, s=3)
+    plt.xlabel("Julian Days")
+    plt.ylabel("Lc Intensity")
+    plt.show()
+    sys.exit(0)
+
 P = find_transit_period(valley_x, valley_y)
 allx, ally = phase_shift(valley_x, allx, ally, P)
 
+# Take a running mean of each list
 N = 551
 meanx = np.convolve(allx, np.ones((N,))/N, mode='valid')[(N-1):]
 meany = np.convolve(ally, np.ones((N,))/N, mode='valid')[(N-1):]
 
+
+# If the graph is a scatter showing a nice U with a magenta line cutting
+# right through it showing the pattern of transit covering you've got a planet.
 plt.scatter(allx, ally, s=1)
 plt.plot(meanx, meany, "m")
 # plt.scatter(valley_x, valley_y, marker='o', color='green', label='Valleys')
