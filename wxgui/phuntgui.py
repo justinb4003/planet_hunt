7472b01b22187669a6e3d0b256c88af3db78dcfe
Justin Buist <justinb@countrysidegreenhouse.com>
2019-11-13 21:44:26 -0500

    Start early layout
    Small left pane for loading raw data into memory, middle large pane for
    visiualization, and right pane for data manipulation tools is my current
    thinking.

--- wxgui/phuntgui.py
+++ wxgui/phuntgui.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import wx
 import sqlalchemy as sa
-import psycopg2 as pg
 
+# BEGIN DATA ROUTINES ###
 sa_engine = sa.create_engine(
                 'postgresql+psycopg2://kuser:kpass@192.168.1.125/kepler')
 metadata = sa.MetaData()
@@ -37,22 +37,57 @@ def load_kepler_id_from_db(kepid):
     print("now returning them.")
     return x, y
 
+# END DATA ROUTINES ###
 
-class justin(wx.Frame):
+
+class DataSearchPanel(wx.Panel):
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent)
+        hbox = wx.BoxSizer(wx.VERTICAL)
+        self.kepid = wx.TextCtrl(self)
+        bload = wx.Button(self, label='Load Kepler ID')
+
+        hbox.Add(self.kepid, 0,
+                 wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
+        hbox.Add(bload, 0,
+                 wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
+        self.Bind(wx.EVT_BUTTON, self.load_data, bload)
+        self.SetSizer(hbox)
+
+    def load_data(self, event):
+        print("hit load_data().: {}".format(self.kepid.GetValue()))
+
+
+class DataDisplayPanel(wx.Panel):
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent)
+        # Need to put something here, right?
+        pass
+
+
+class MainWindow(wx.Frame):
 
     def __init__(self, parent, id):
-        wx.Frame.__init__(self, parent, id, 'Title', size=(300, 200))
-        lpanel = wx.Panel(self)
-        bload_data = wx.Button(lpanel, label='Load Data', pos=(100, 100),
-                               size=(60, 60))
-        self.Bind(wx.EVT_BUTTON, self.load_data, bload_data)
-        self.Bind(wx.EVT_CLOSE, self.close_window)
+        wx.Frame.__init__(self, parent, id, 'Planet Hunt', size=(800, 600))
+
+        sp1 = wx.SplitterWindow(self)
+        data_search_panel = DataSearchPanel(sp1)
+        data_display_panel = DataDisplayPanel(sp1)
+        sp1.SplitVertically(data_search_panel, data_display_panel)
+        sp1.SetMinimumPaneSize(200)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(sp1, 1, wx.EXPAND)
+        self.SetSizer(sizer)
 
         status_bar = self.CreateStatusBar()
         menubar_main = wx.MenuBar()
         file_menu = wx.Menu()
         edit_menu = wx.Menu()
-        file_menu.Append(wx.NewIdRef(), 'Connect...', 'Connect to a new server')
+        file_menu.Append(wx.NewIdRef(), 'Connect...',
+                         'Connect to a new server')
         file_menu.Append(wx.NewIdRef(), 'Close', 'Quit the application')
         menubar_main.Append(file_menu, 'File')
         menubar_main.Append(edit_menu, 'Edit')
@@ -74,6 +109,6 @@ class justin(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
-    frame = justin(parent=None, id=-1)
+    frame = MainWindow(parent=None, id=-1)
     frame.Show()
     app.MainLoop()
