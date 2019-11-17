bbaedccbde7c4298dd448b3089e54de5208d84ef
Justin Buist <justinb@countrysidegreenhouse.com>
2019-11-16 22:47:55 -0500

    Add comobox for planet selection

--- wxgui/phuntgui.py
+++ wxgui/phuntgui.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import wx
 import numpy as np
-import matplotlib as mp
 import sqlalchemy as sa
 
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
@@ -17,7 +16,7 @@ sa_engine = sa.create_engine(
                 'postgresql+psycopg2://kuser:kpass@192.168.1.125/kepler')
 metadata = sa.MetaData()
 NASAResult = namedtuple('NASAResult', 'kepoi_name kepler_name period '
-                                      'first_transit')
+                                      'first_transit koi_disposition')
 
 OBJECT_X = []
 OBJECT_Y = []
@@ -70,7 +69,7 @@ def get_accepted_result_from_db(kepid):
     ret = []
     for row in rs:
         res = NASAResult(row.kepoi_name, row.kepler_name, row.koi_period,
-                         row.koi_time0bk)
+                         row.koi_time0bk, row.koi_disposition)
         ret.append(res)
     return ret
 
@@ -109,31 +108,63 @@ class DataSearchPanel(wx.Panel):
         self.kepid = wx.TextCtrl(self)
         bload = wx.Button(self, label='Load Kepler ID')
 
-        self.kepname = wx.StaticText(self)
-        self.result_period = wx.StaticText(self)
-        self.kepname.SetLabel('N/A')
-        self.result_period.SetLabel('0.00')
+        self.kep_choice_ddl = wx.ComboBox(self, -1, choices=[],
+                                          style=wx.CB_READONLY)
+        self.kep_choice_ddl.Bind(wx.EVT_COMBOBOX, self.on_kep_choice)
+        # self.kep_choice_ddl.Bind(wx.EVT_TEXT, self.on_kep_choice)
 
-        self.kepid.SetValue(str('6922244'))  # Nice dummy value
+        self.kepoi_name = wx.StaticText(self)
+        self.kepler_name = wx.StaticText(self)
+        self.result_period = wx.StaticText(self)
+        self.first_transit = wx.StaticText(self)
+        self.koi_disposition = wx.StaticText(self)
+        self.kepoi_name.SetLabel('N/A')
+        self.kepler_name.SetLabel('N/A')
+        self.result_period.SetLabel('0.00')
+        self.first_transit.SetLabel('0.00')
+        self.koi_disposition.SetLabel('N/A')
+
+        self.kepid.SetValue(str('10984090'))  # A binary system
+        # self.kepid.SetValue(str('6922244'))  # A single system
 
         hbox.Add(self.kepid, 0,
                  wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
         hbox.Add(bload, 0,
                  wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
         self.Bind(wx.EVT_BUTTON, self.load_data, bload)
-        hbox.Add(self.kepname, 0,
+        hbox.Add(self.kep_choice_ddl, 0,
+                 wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
+        hbox.Add(self.kepoi_name, 0,
+                 wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
+        hbox.Add(self.kepler_name, 0,
                  wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
         hbox.Add(self.result_period, 0,
                  wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
+        hbox.Add(self.first_transit, 0,
+                 wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
+        hbox.Add(self.koi_disposition, 0,
+                 wx.ALIGN_CENTER_HORIZONTAL | wx.ALL | wx.EXPAND, 5)
         self.SetSizer(hbox)
+
+    def on_kep_choice(self, event):
+        print('fire')
+        choice = self.kep_choice_ddl.GetValue()
+        for r in [x for x in self.result_list if x.kepoi_name == choice]:
+            self.kepoi_name.SetLabel(str(r.kepoi_name))
+            self.kepler_name.SetLabel(str(r.kepler_name))
+            self.result_period.SetLabel(str(r.period))
+            self.first_transit.SetLabel(str(r.first_transit))
+            self.koi_disposition.SetLabel(str(r.koi_disposition))
+            self.modify_panel.period.SetValue(str(r.period))
+            self.modify_panel.first_transit.SetValue(str(r.first_transit))
+            print("Target period(s): {}".format(r.period))
+            print("Target 0k(s): {}".format(r.first_transit))
 
     def load_data(self, event):
         kepid = None
         try:
             kepid = int(self.kepid.GetValue())
         except ValueError:
-            # BAD: Just shove a debug value in for now.
-            # TODO: MsgBox
             wx.MessageBox('Unable to read Kepler ID', 'Problem!',
                           wx.OK | wx.ICON_INFORMATION)
 
@@ -141,17 +172,19 @@ class DataSearchPanel(wx.Panel):
             print("hit load_data().: {}".format(kepid))
             global OBJECT_X, OBJECT_Y
             OBJECT_X, OBJECT_Y = load_kepler_id_from_db(kepid)
-            result_list = get_accepted_result_from_db(kepid)
-            for r in result_list:
-                self.result_period.SetLabel(str(r.period))
-                self.kepname.SetLabel(str(r.kepler_name))
-                self.modify_panel.period.SetValue(str(r.period))
-                self.modify_panel.first_transit.SetValue(str(r.first_transit))
-                print("Target period(s): {}".format(r.period))
-                print("Target 0k(s): {}".format(r.first_transit))
+            self.result_list = get_accepted_result_from_db(kepid)
+            kep_choice = [r.kepoi_name for r in self.result_list]
+            kep_choice.sort()
 
+            self.kep_choice_ddl.Clear()
+            self.kep_choice_ddl.AppendItems(kep_choice)
+            # I'm intentionally not defaulting the drop-down to a default
+            # selection because I don't like being shown the known data
+            # without taking action.  Personal preferance.
+            # .. but I still want to know why this doesn't work.
+            # self.kep_choice_ddl.SetValue(kep_choice[0])
             self.display_panel.display_transit_raw(OBJECT_X, OBJECT_Y)
-            print("You should see the magic now.")
+            print("You should see the raw transit now.")
 
 
 class DataDisplayPanel(wx.Panel):
@@ -184,6 +217,7 @@ class DataDisplayPanel(wx.Panel):
         self.force_update()
 
     def force_update(self):
+        # TODO: This isn't always forcing a redraw on some of my computers.
         self.canvas.draw()
         self.Layout()
 
@@ -193,7 +227,6 @@ class DataModifyPanel(wx.Panel):
     def __init__(self, parent, display_panel):
         wx.Panel.__init__(self, parent=parent)
         self.display_panel = display_panel
-        self.SetSize((100, -1))
         hbox = wx.BoxSizer(wx.VERTICAL)
         self.period = wx.TextCtrl(self)
         self.first_transit = wx.TextCtrl(self)
@@ -221,9 +254,6 @@ class DataModifyPanel(wx.Panel):
         x, y = phase_shift(ftrans, OBJECT_X, OBJECT_Y, period)
         # Take a running mean of N size for each list
         N = 551  # <-- magic number found via fiddling
-        # meanx = np.convolve(x, np.ones((N,))/N, mode='valid')[(N-1):]
-        # meany = np.convolve(y, np.ones((N,))/N, mode='valid')[(N-1):]
-
         meanx = running_mean(x, N)
         meany = running_mean(y, N)
 
@@ -239,7 +269,7 @@ class DataModifyPanel(wx.Panel):
 class MainWindow(wx.Frame):
 
     def __init__(self, parent, id):
-        wx.Frame.__init__(self, parent, id, 'Planet Hunt', size=(1000, 600))
+        wx.Frame.__init__(self, parent, id, 'Planet Hunt', size=(1400, 600))
 
         splitter = MultiSplitterWindow(self, style=wx.SP_LIVE_UPDATE)
         # The passing references to panels and controsl to each other
@@ -249,7 +279,7 @@ class MainWindow(wx.Frame):
         data_search_panel = DataSearchPanel(splitter, data_display_panel,
                                             data_modify_panel)
         splitter.AppendWindow(data_search_panel, sashPos=150)
-        splitter.AppendWindow(data_display_panel, sashPos=700)
+        splitter.AppendWindow(data_display_panel, sashPos=1050)
         splitter.AppendWindow(data_modify_panel)
 
         status_bar = self.CreateStatusBar()
@@ -266,15 +296,6 @@ class MainWindow(wx.Frame):
 
     def close_window(self, event):
         self.Destroy()
-
-    def load_data(self, event):
-        msg = wx.TextEntryDialog(None, 'Enter Kepler ID to view', 'Enter ID',
-                                 '')
-        if msg.ShowModal() == wx.ID_OK:
-            kepid = msg.GetValue()
-            load_kepler_id_from_db(kepid)
-
-        msg.Destroy()
 
 
 if __name__ == '__main__':
